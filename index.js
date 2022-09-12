e6631d774b90cc5ad029737760e91a73ccb8281d
Lordo <lordocreations@gmail.com>
2022-09-12 16:15:24 -0700

    update

--- index.js
+++ index.js
@@ -1,15 +1,25 @@
-const https = require('https');
+const cheerio = require("cheerio");
+const got = require("got");
 
+const express = require("express");
+const app = express();
+const port = 3000;
 
-const reqstr = `
-https://cemc2.math.uwaterloo.ca/contest/PSG/school/print.php?f=web&h=y&t=&ids=p${Math.floor(Math.random() * 2100)}
-`
+app.get("/", async (reque, qres) => {
+  console.log("Request recieved");
+  const reqstr = `
+https://cemc2.math.uwaterloo.ca/contest/PSG/school/print.php?f=web&h=y&t=&ids=p${Math.floor(
+    Math.random() * 2100
+  )}`;
 
-const req = https.request(reqstr, res => {
-	const data = [];
+  const response = await got(reqstr, { json: false });
+  const $ = cheerio.load(response.body);
 
-	res.on('data', _ => data.push(_))
-	res.on('end', () => console.log(data.join()))
+  console.log($("head").html()+$(".problem").html());
+  qres.send($("head").html()+$(".problem").html())
+  // qres.send($(".problem").html())
 });
 
-req.end();
+app.listen(port, () => {
+  console.log(`App listening on port ${port}`);
+});
