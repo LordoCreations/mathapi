4719d9da5fd4e9b701d3bf02263d51344b037433
Lordo <lordocreations@gmail.com>
2022-09-13 16:51:57 -0700

    slim down

--- index.js
+++ index.js
@@ -7,10 +7,11 @@ const cors = require("cors");
 
 const express = require("express");
 const app = express();
-const port = 3000;
+const port = 8000;
 
 app.use(cors({ origin: process.env.REMOTE_CLIENT_APP, credentials: true }));
 
+const heading = `<head><style type="text/css" media="all"> body{font-family: MathJax_MainRegular; font-size: 14px; /***font-weight: 300; color: #414141;***/ line-height: 1.3; letter-spacing: 0.5px; margin: 0 auto; width: 600px; background: white; zoom: 1;}table{page-break-after: auto}tr{page-break-inside: avoid; page-break-after: auto}td{page-break-inside: avoid; page-break-after: auto}div{page-break-inside: avoid; page-break-after: auto}.problem{page-break-inside: avoid; page-break-after: auto}</style> <script type="text/x-mathjax-config"> MathJax.Hub.Config({tex2jax:{inlineMath: [ ['$','$'], ["\\(","\\)"]], displayMath: [ ['$$','$$'], ["\\[","\\]"]], processEscapes: true}});</script> <script type="text/javascript" async src="https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.1/MathJax.js?config=TeX-AMS_SVG"></script></head>`
 
 app.get("/", async (reque, qres) => {
   console.log("Request recieved");
@@ -22,8 +23,7 @@ https://cemc2.math.uwaterloo.ca/contest/PSG/school/print.php?f=web&h=y&t=&ids=p$
   const response = await got(reqstr, { json: false });
   const $ = cheerio.load(response.body);
   $('title').remove();
-
-  let parsed = $("head").html()+$(".problem").html()
+  let parsed = $(heading)+$(".problem").html()
 
   const $$ = cheerio.load(parsed);
 
