b102be3a81276ab8176e567b6bed5214e384e37a
Lordo <lordocreations@gmail.com>
2022-09-14 16:46:52 -0700

    safe-api

--- index.js
+++ index.js
@@ -34,6 +34,27 @@ https://cemc2.math.uwaterloo.ca/contest/PSG/school/print.php?f=web&h=y&t=&ids=p$
   // qres.send($(".problem").html())
 });
 
+app.get("/p", async (reque, qres) => {
+  console.log("Request recieved");
+  const reqstr = `
+https://cemc2.math.uwaterloo.ca/contest/PSG/school/print.php?f=web&h=y&t=&ids=p${Math.floor(
+    Math.random() * 2100
+  )}`;
+
+  const response = await got(reqstr, { json: false });
+  const $ = cheerio.load(response.body);
+  $('title').remove();
+  let parsed = $(".problem").html()
+
+  const $$ = cheerio.load(parsed);
+
+        
+  // console.log($$.html())
+  // console.log($("head").html()+$(".problem").html());
+  qres.send($$.html())
+  // qres.send($(".problem").html())
+});
+
 app.listen(port, () => {
   console.log(`App listening on port ${port}`);
 });
